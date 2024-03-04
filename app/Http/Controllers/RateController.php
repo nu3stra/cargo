63597f7f673e3afb7aec46216c41c758fe8f9406
Roman Knyazev <roman.ecl.dev@gmail.com>
2024-03-04 16:34:08 +0400

    added admin panel, added routes models and migrations

---
+++ app/Http/Controllers/RateController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class RateController extends Controller
+{
+    public function __invoke()
+    {
+        // TODO: Implement __invoke() method.
+    }
+
+    public function index()
+    {
+
+    }
+
+    public function create()
+    {
+
+    }
+
+    public function store()
+    {
+
+    }
+
+    public function show()
+    {
+
+    }
+
+    public function edit()
+    {
+
+    }
+
+    public function update()
+    {
+
+    }
+
+    public function destroy()
+    {
+
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use TCG\Voyager\Voyager;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,4 +16,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+
+Route::group(['prefix' => 'admin'], function () {
+    $voyager = new Voyager();
+    $voyager->routes();
 });
